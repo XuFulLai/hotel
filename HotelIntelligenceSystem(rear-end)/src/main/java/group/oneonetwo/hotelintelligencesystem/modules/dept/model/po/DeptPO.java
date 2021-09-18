8152d5332b8ec6c13876c2bddc0fd80a00942196
Tomato-Simon <1499602163@qq.com>
2021-09-18 22:57:57 +0800

    refactor: 重构PO自动填充

--- HotelIntelligenceSystem(rear-end)/src/main/java/group/oneonetwo/hotelintelligencesystem/modules/dept/model/po/DeptPO.java
+++ HotelIntelligenceSystem(rear-end)/src/main/java/group/oneonetwo/hotelintelligencesystem/modules/dept/model/po/DeptPO.java
@@ -1,5 +1,6 @@
 package group.oneonetwo.hotelintelligencesystem.modules.dept.model.po;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
