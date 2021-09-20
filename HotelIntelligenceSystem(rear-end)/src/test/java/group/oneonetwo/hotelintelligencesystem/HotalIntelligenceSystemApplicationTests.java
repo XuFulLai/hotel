e5ee0b24a7d42878ad246906a070d65fa54e878a
Tomato-Simon <1499602163@qq.com>
2021-09-20 13:19:29 +0800

    feat: 增加转换工具类(list转换,page转换)

--- HotelIntelligenceSystem(rear-end)/src/test/java/group/oneonetwo/hotelintelligencesystem/HotalIntelligenceSystemApplicationTests.java
+++ HotelIntelligenceSystem(rear-end)/src/test/java/group/oneonetwo/hotelintelligencesystem/HotalIntelligenceSystemApplicationTests.java
@@ -1,13 +1,33 @@
 package group.oneonetwo.hotelintelligencesystem;
 
+import group.oneonetwo.hotelintelligencesystem.modules.user.model.po.UserPO;
+import group.oneonetwo.hotelintelligencesystem.modules.user.model.vo.UserVO;
+import group.oneonetwo.hotelintelligencesystem.tools.ConvertUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class HotalIntelligenceSystemApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void listConvertTest() {
+        List<UserPO> userPOS = new ArrayList<>();
+        List<UserVO> userVOS = new ArrayList<>();
+        UserPO userPO = new UserPO();
+        userPO.setId("123");
+        userPO.setDept("456");
+        userPOS.add(userPO);
+
+        List<UserVO> userVOS1 = ConvertUtil.transforList(userPOS, UserVO.class);
+        System.out.println(userVOS1);
+
     }
 
 }
