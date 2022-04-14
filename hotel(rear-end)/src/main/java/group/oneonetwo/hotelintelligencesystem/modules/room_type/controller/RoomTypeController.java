273453c1f1f3a3ef958b3ee02ff1deebb56dc115
Tomato-Simon <1499602163@qq.com>
2022-04-14 23:23:28 +0800

    feat: 增加门户酒店详情页

--- hotel(rear-end)/src/main/java/group/oneonetwo/hotelintelligencesystem/modules/room_type/controller/RoomTypeController.java
+++ hotel(rear-end)/src/main/java/group/oneonetwo/hotelintelligencesystem/modules/room_type/controller/RoomTypeController.java
@@ -1,6 +1,7 @@
 package group.oneonetwo.hotelintelligencesystem.modules.room_type.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import group.oneonetwo.hotelintelligencesystem.modules.room.model.vo.RoomVO;
 import group.oneonetwo.hotelintelligencesystem.modules.room.service.IRoomService;
 import group.oneonetwo.hotelintelligencesystem.modules.room_type.model.vo.RoomTypeVO;
 import group.oneonetwo.hotelintelligencesystem.modules.room_type.service.IRoomTypeServeice;
@@ -12,6 +13,8 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "房间类型相关接口")
 @RestController
@@ -51,5 +54,11 @@ public class RoomTypeController {
     @ApiOperation("获取房间类型列表(分页)")
     public Reply<Page<RoomTypeVO>> getPage(@RequestBody RoomTypeVO roomTypeVO){
         return Reply.success(roomTypeServeice.getPage(roomTypeVO));
+    }
+
+    @GetMapping("currentRoomTypeList/{id}")
+    @ApiOperation("获取当前酒店现有房间类型列表")
+    public  Reply<List<RoomTypeVO>> currentRoomTypeList(@PathVariable("id") String id){
+        return Reply.success(roomTypeServeice.currentRoomTypeList(id));
     }
 }
